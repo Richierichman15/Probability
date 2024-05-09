a3f9ead71e0c07f3438a4410573b3915d68187f9
Gitonga <richierichman15@gmail.com>
2024-05-09 09:38:21 -0500

    wip

---
+++ gamble/src/components/math.js
@@ -0,0 +1,21 @@
+import react from "react";
+
+const math = () => {
+    const factorial = n => {
+        if ( n === 0 || n === 1) return 1;
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            return *= i;
+        }
+        return result
+    }
+    return(
+        <div>
+            <h3>Probability</h3>
+            <input></input>
+            <button></button>
+        </div>
+    )
+}
+
+export default math;
